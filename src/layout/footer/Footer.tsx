51a27f27f623878c4069c72749e985cf4dfdfbb5
eugeneman <evgensbk9@gmail.com>
2023-12-02 13:55:39 +0700

    refactoring footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,113 +1,48 @@
 import React from 'react';
 import { Icon } from "../../component/icon/Icon";
-import styled from "styled-components";
 import { FlexWrapper } from "../../component/FlexWrapper";
-import { Button } from "../../component/Button";
-import { theme } from "../../styles/Theme";
 import { Container } from "../../component/Container";
-import { Link } from "../../component/Link";
-import { Main } from "../sections/main/Main";
-import { font } from '../../styles/Common';
+import { S } from './Footer_Styles';
+
+const socialItemsData = [
+  {
+    iconId: "telegrm"
+  },
+  {
+    iconId: "vk"
+  },
+  {
+    iconId: "insta"
+  },
+  {
+    iconId: "linkedin"
+  }
+]
 
 export const Footer = () => {
   return (
-    <StyledFooter>
+    <S.Footer>
       <Container>
         <FlexWrapper direction={"column"} align={"center"}>
-          <Invite>Connect with me</Invite>
-          <SocialList>
-            <SocialItem>
-              <SocialLink>
-                <Icon iconId={"telegrm"} width={"21px"} height={"21px"} viewBox={"0 0 21 21"} />
-              </SocialLink>
-            </SocialItem>
-            <SocialItem>
-              <SocialLink>
-                <Icon iconId={"vk"} width={"21px"} height={"21px"} viewBox={"0 0 21 21"} />
-              </SocialLink>
-            </SocialItem>
-            <SocialItem>
-              <SocialLink>
-                <Icon iconId={"insta"} width={"21px"} height={"21px"} viewBox={"0 0 21 21"} />
-              </SocialLink>
-            </SocialItem>
-            <SocialItem>
-              <SocialLink>
-                <Icon iconId={"linkedin"} width={"21px"} height={"21px"} viewBox={"0 0 21 21"} />
-              </SocialLink>
-            </SocialItem>
-          </SocialList>
-          <GoTopLink>
+          <S.Invite>Connect with me</S.Invite>
+          <S.SocialList>
+            {socialItemsData.map((item, index) => {
+            return (
+              <S.SocialItem>
+                <S.SocialLink key={index}>
+                  <Icon iconId={item.iconId} width={"21px"} height={"21px"} viewBox={"0 0 21 21"} />
+                </S.SocialLink>
+              </S.SocialItem>
+            )})}
+          </S.SocialList>
+          <S.GoTopLink>
             <Icon iconId={"goTop"} width={"30px"} height={"30px"} viewBox={"0 0 30 30"} />
-          </GoTopLink>
-          <Copyright>© 2023 Designed by Evgeny Markin, All Rights Reserved.</Copyright>
+          </S.GoTopLink>
+          <S.Copyright>© 2023 Designed by Evgeny Markin, All Rights Reserved.</S.Copyright>
         </FlexWrapper>
       </Container>
-    </StyledFooter>
+    </S.Footer >
   );
 };
 
-const StyledFooter = styled.footer`
-  padding:40px 0;
-  position:relative;
-  
-  
-  ${Button}{
-    align-self: flex-start;
-    position:absolute;
-    top: 20px;
-    left: 20px;
-    
-    @media ${theme.media.mobile}{
-      padding:6px 8px;
-    }
-  }
-  
-`
-const Invite = styled.span`
-  ${font({ weight: 500, Fmax: 16, Fmin: 12 })};  
-`
 
-const SocialList = styled.ul`
-  display: flex;
-  gap: 20px;
-  margin:20px 0;    
-`
-const SocialItem = styled.li`
-
-`
-const SocialLink = styled.a`
-  background-color:${theme.colors.miniBlock};
-  color:${theme.colors.font};
-  border-radius: 50%;
-  width: 35px;
-  height: 35px;
-  
-  display:flex;
-  justify-content: center;
-  align-items: center;  
-  
-  &:hover{
-    background:linear-gradient(90deg,#00F5A0 0%,#00D9F5 100%);
-    color:${theme.colors.button};
-    transform: translateY(-4px);
-  }
-`
-const GoTopLink = styled.a`
-  position: absolute;
-  right:15px;
-  bottom:10px;
-
-  &:hover{
-    color:${theme.colors.accent};
-  }  
-`
-const Copyright = styled.small`
-  ${font({ lineHeight: 1.5, Fmax: 12, Fmin: 6 })};
-  /* font-weight: 400;
-  font-size:12px;
-  line-height: 1.5; */
-  text-align: center;
-  opacity: 0.5;
-  
-`
---
+++ src/layout/footer/Footer_Styles.ts
@@ -0,0 +1,72 @@
+import styled from "styled-components"
+import { font } from "../../styles/Common"
+import { theme } from "../../styles/Theme"
+import { Button } from "../../component/Button"
+
+const Footer = styled.footer`
+  padding:40px 0;
+  position:relative;  
+  
+  ${Button}{
+    align-self: flex-start;
+    position:absolute;
+    top: 20px;
+    left: 20px;
+    
+    @media ${theme.media.mobile}{
+      padding:6px 8px;
+    }
+  }  
+`
+const Invite = styled.span`
+  ${font({ weight: 500, Fmax: 16, Fmin: 12 })};  
+`
+
+const SocialList = styled.ul`
+  display: flex;
+  gap: 20px;
+  margin:20px 0;    
+`
+const SocialItem = styled.li`
+
+`
+const SocialLink = styled.a`
+  background-color:${theme.colors.miniBlock};
+  color:${theme.colors.font};
+  border-radius: 50%;
+  width: 35px;
+  height: 35px;
+  
+  display:flex;
+  justify-content: center;
+  align-items: center;  
+  
+  &:hover{
+    background:linear-gradient(90deg,#00F5A0 0%,#00D9F5 100%);
+    color:${theme.colors.button};
+    transform: translateY(-4px);
+  }
+`
+const GoTopLink = styled.a`
+  position: absolute;
+  right:15px;
+  bottom:10px;
+
+  &:hover{
+    color:${theme.colors.accent};
+  }  
+`
+const Copyright = styled.small`
+  ${font({ lineHeight: 1.5, Fmax: 12, Fmin: 6 })};
+  text-align: center;
+  opacity: 0.5;  
+`
+export const S = {
+    Footer,
+    Invite,
+    SocialList,
+    SocialItem,
+    SocialLink,
+    GoTopLink,
+    Copyright
+}
